4164106265d54d709858727b5ddf74bcaf08c5c4
Avaneesh prajapati <prajapatiavaneesh1998@gmail.com>
2020-12-22 12:59:55 +0530

    initials commit for clothe-store

---
+++ clothe-store/src/components/menu-item/menu-item.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const MenuItem = () => {
+    return (  
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Hats</h1>
+                <span className='subtitle'>Shop Now</span>
+            </div>
+        </div> 
+    );
+}
+ 
+export default MenuItem;
